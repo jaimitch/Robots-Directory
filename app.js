fa9fe79a386b418b68435d6fcee8154dc86c1f96
Jaren Mitchell <jaren_mitchell@yahoo.com>
2017-07-18 13:55:03 -0500

    something is not working in the robots.mustache with the {{id}} closing tag and I can't figure it out

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const robots = require('./data.js');
+
+const application = express();
+
+application.engine('mustache', mustacheExpress());
+application.set('view engine', 'mustache' );
+application.set('views', './views');
+
+
+application.use(express.static('public'));
+
+application.get('/', function (request, response) {
+    console.log(robots);
+    response.render('index', robots);
+  
+});
+
+// application.get('/', function (request, response) {
+//     response.render('robots', robots);
+// });
+
+application.get('/:userInfo', (request, response) => {
+    var user = robots.users[request.params.userInfo -1]
+    response.render('robots', user);
+});
+
+
+// application.listen(3000);
